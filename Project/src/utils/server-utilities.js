eb752c2824e0919ae3494896389ddaea6f91e648
Rishav Raj <rishavrajaryan14@gmail.com>
2021-10-28 01:24:56 +0530

    structure added with few pages

---
+++ Project/src/utils/server-utilities.js
@@ -0,0 +1,135 @@
+const mongodb = require('mongodb');
+let foodAvailability = 0;
+let time = 0;
+
+const addUser = function(username, email, password, type, restaurent=null, db){
+    db.collection('users').insertOne({
+        username: username,
+        password: password,
+        email: email,
+        type: type,
+        restaurent: restaurent
+    }, function(error, result){
+        if(error){
+            return console.log("Unable to add User");
+        }
+
+        //console.log(result.ops);
+    });
+}
+
+const loginUser = function(username, password, type,db){
+    db.collection('users').findOne({
+        username: username,
+        password: password,
+        type: type
+    }, function(error, user){
+        if (error){
+            return console.log("[-] No Such User Exists");
+        }
+
+        //console.log(user);
+    })
+}
+
+const updateOrders = function(foodName, restaurent,db){
+    db.collection('current-orders').find({
+        foodName: foodName,
+        restaurent: restaurent
+    }).toArray(function(error, result){
+        if (result){
+            //console.log("**********Result*********\n", result);
+            db.collection('current-orders').updateMany({
+                foodName: foodName,
+                restaurent: restaurent
+            }, {
+                $set: {
+                    count: result.length
+                }
+            })
+        }
+    })
+}
+
+const checkFoodPresent = function(foodName, db){
+    db.collection('catalog').findOne({
+        foodName: foodName
+    }, function(error, food){
+        if(error){
+            return console.log("[-] Unable To Access Database");
+        }
+        if (!food){
+            foodAvailability = 0;
+        }else{
+            foodAvailability = 1
+        }
+    });
+}
+
+const addFood = function(foodName, timeToPreapare, imageUrl, price, tags, preaparation, ingredients, restaurent,db){
+    checkFoodPresent(foodName, db);
+    if(foodAvailability === 1){
+        return console.log("[-] Food already Exists")
+    }
+    db.collection('catalog').insertOne({
+        foodName: foodName,
+        timeToPreapare: timeToPreapare,
+        imageUrl: imageUrl,
+        price: price,
+        discount: 0,
+        rating: 0,
+        numberOfTimesOrdered: 0,
+        tags: tags,
+        preaparation: preaparation,
+        ingredients: ingredients,
+        restaurent: restaurent
+    });
+}
+
+const addOrder = function(customerName, foodName, restaurent, db, callback){
+    // Fetching Time To Preapare Food From Database
+    db.collection('catalog').findOne({
+        foodName: foodName
+    }, function(error, food){
+        if (error){
+            return console.log("[-] Unable to fetch Time To Preapare Food");
+        }
+
+        time = food.timeToPreapare;
+        // console.log("******Food********\n", food);
+        // console.log("******FoodPrepTime********* ----> ", time);
+        callback(customerName, foodName, time, restaurent, db);
+    })
+}
+
+const deleteFoodItem = function(foodName, restaurent, db){
+    db.collection('catalog').deleteOne({
+        foodName: foodName,
+        restaurent: restaurent
+    }).then(function(result){
+        console.log('The Result is --->\n', result)
+    }).catch(function(error){
+        console.log('The error is --->\n', error);
+    })
+}
+
+const getRestaurentFood = function(restaurent, db, callback){
+    db.collection('catalog').find({
+        restaurent: restaurent
+    }).toArray(function(error, foodItems){
+        if (error){
+            return console.log('Unable to fetch data');
+        }
+        callback(foodItems);
+    });
+}
+
+module.exports = {
+    addUser: addUser,
+    loginUser: loginUser,
+    addOrder: addOrder,
+    updateOrders: updateOrders,
+    addFood: addFood,
+    deleteFoodItem: deleteFoodItem,
+    getRestaurentFood: getRestaurentFood
+}
